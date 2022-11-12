28de37e9ad1bcd56647daa3b97b6fcba67c19e61
Vadim <croft9289@gmail.com>
2022-11-12 16:22:33 +0300

    db connection test

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("подключение субд")
+
+	db, err := sql.Open("mysql", "root:root@/mydbtest")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
+
+	fmt.Println("Connected")
 }
