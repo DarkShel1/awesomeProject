799e408da14f15e92dff40247488e8d1a90db6c8
Vadim <croft9289@gmail.com>
2022-11-16 23:25:39 +0300

    basic server functions

--- model/models.go
+++ model/models.go
@@ -3,6 +3,11 @@ package model
 type User struct {
 	ID    string `json:"id"`
 	Name  string `json:"name"`
-	Age   uint32 `json:"age"`
+	Age   string `json:"age"`
 	Email string `json:"email"`
+}
+
+type Balance struct {
+	UserID  int32 `json:"user_id"`
+	Balance int32 `json:"balance"`
 }
