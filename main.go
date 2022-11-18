c7df925bbc64ecb926a6b405e34475368f601903
Vadim <croft9289@gmail.com>
2022-11-19 00:09:20 +0300

    main

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 			ServerName: "localhost:3306",
 			User:       "root",
 			Password:   "root",
-			DB:         "dbName",
+			DB:         "DbName",
 		}
 
 	connectionString := database.GetConnectionString(config)
@@ -26,20 +26,28 @@ func main() {
 	log.Println("HTTP server starts on port 8090")
 	router := mux.NewRouter()
 
+	/*создание баланса юзеров*/
 	router.HandleFunc("/create", createBalance).Methods("POST")
 
+	/*получение баланса юзеров по ID*/
 	router.HandleFunc("/get/{id}", getBalanceByID).Methods("GET")
 
+	/*изменение баланса юзеров по ID*/
 	router.HandleFunc("/update/{id}", updateBalanceByID).Methods("PUT")
 
+	/*удаление баланса юзеров по ID*/
 	router.HandleFunc("/delete/{id}", deleteBalanceByID).Methods("DELETE")
 
+	/*создание резервирования суммы*/
 	router.HandleFunc("/create_reservation", createReservation).Methods("POST")
 
+	/*получение резервирования суммы по ID*/
 	router.HandleFunc("/get_reservation/{id}", getReservationByID).Methods("GET")
 
+	/*изменение резервирования суммы по ID*/
 	router.HandleFunc("/update_reservation/{id}", updateReservationByID).Methods("PUT")
 
+	/*удаление резервирования суммы по ID*/
 	router.HandleFunc("/delete_reservation/{id}", deleteReservationByID).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8090", router))
