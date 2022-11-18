6ad0b470e9d8e90384d8b7af7a868648f74e4133
Vadim <croft9289@gmail.com>
2022-11-19 00:09:20 +0300

    main

--- main.go
+++ main.go
@@ -26,21 +26,21 @@ func main() {
 	log.Println("HTTP server starts on port 8090")
 	router := mux.NewRouter()
 
-	router.HandleFunc("/create", createBalance).Methods("POST")
+	router.HandleFunc("/create", createBalance).Methods("POST") //создание баланса юзеров
 
-	router.HandleFunc("/get/{id}", getBalanceByID).Methods("GET")
+	router.HandleFunc("/get/{id}", getBalanceByID).Methods("GET") //получение баланса юзеров по ID
 
-	router.HandleFunc("/update/{id}", updateBalanceByID).Methods("PUT")
+	router.HandleFunc("/update/{id}", updateBalanceByID).Methods("PUT") //изменение баланса юзеров по ID
 
-	router.HandleFunc("/delete/{id}", deleteBalanceByID).Methods("DELETE")
+	router.HandleFunc("/delete/{id}", deleteBalanceByID).Methods("DELETE") //удаление баланса юзеров по ID
 
-	router.HandleFunc("/create_reservation", createReservation).Methods("POST")
+	router.HandleFunc("/create_reservation", createReservation).Methods("POST") //создание резервирования суммы
 
-	router.HandleFunc("/get_reservation/{id}", getReservationByID).Methods("GET")
+	router.HandleFunc("/get_reservation/{id}", getReservationByID).Methods("GET") //получение резервирования суммы по ID
 
-	router.HandleFunc("/update_reservation/{id}", updateReservationByID).Methods("PUT")
+	router.HandleFunc("/update_reservation/{id}", updateReservationByID).Methods("PUT") //изменение резервирования суммы по ID
 
-	router.HandleFunc("/delete_reservation/{id}", deleteReservationByID).Methods("DELETE")
+	router.HandleFunc("/delete_reservation/{id}", deleteReservationByID).Methods("DELETE") //удаление резервирования суммы по ID
 
 	log.Fatal(http.ListenAndServe(":8090", router))
 }
